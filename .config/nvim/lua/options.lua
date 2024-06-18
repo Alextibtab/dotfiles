55b6de57ffa368c3c7d0a6e56d03cb620d83d111
Alextibtab <14alexcockburn@gmail.com>
2024-06-18 21:37:39 +0100

    Added shell check for windows or unix

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -21,7 +21,22 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
-vim.opt.shell = 'bash -l'
+if vim.fn.has 'win32' then
+  local powershell_options = {
+    shell = vim.fn.executable 'pwsh' == 1 and 'pwsh' or 'powershell',
+    shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+    shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait',
+    shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode',
+    shellquote = '',
+    shellxquote = '',
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+else
+  vim.opt.shell = 'bash -l'
+end
 
 vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -16,7 +16,7 @@ return {
       notify_on_error = false,
       format_on_save = function(bufnr)
         return {
-          timeout_ms = 500,
+          timeout_ms = 1000,
           lsp_fallback = vim.bo[bufnr].filetype,
         }
       end,
